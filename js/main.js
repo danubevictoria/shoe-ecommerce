ff0756b61092035ab2441110b70090c71dd92b62
Danube Phan <dphan@devonway.com>
2017-06-01 23:32:42 -0700

    update to items

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ var shoes = [];
 
 // Create each shoe for sale
 var mulesandal = new Shoe(
-	'../images/mulesandal.jpg',
+	'images/mulesandal.jpg',
 	'Greer Mule Sandal',
 	'$118.50',
 	'COLOR: warm nutmeg',
@@ -23,18 +23,36 @@ var mulesandal = new Shoe(
 	0
 	);
 
+var alicesandal = new Shoe(
+	'images/alicesandal.jpg',
+	'Alice Sandal',
+	'$118.50',
+	'COLOR: otter suede',
+	4,
+	0
+	);
+
+var talisabucklesandal = new Shoe(
+	'images/talisabucklesandal.jpg',
+	'Talisa Buckle Sandal',
+	'$124.50',
+	'COLOR: english saddle',
+	4,
+	0
+	);
+
 shoes.push(mulesandal);
+shoes.push(alicesandal);
+shoes.push(talisabucklesandal);
 
 // Create each thumbnail component using bootstrap
 for(var i = 0; i < shoes.length; i++) {
 	var wrapper = document.createElement('div');
-	wrapper.className = 'row';
-	wrapper.appendChild(innerdiv);
-
+	// wrapper.className = 'row';
+	
 	var innerdiv = document.createElement('div');
-	innerdiv.className = 'col-sm-6  col-md-4';
-	innerdiv.appendChild(thumbnail);
-
+	innerdiv.className = 'col-sm-4  col-md-2';
+	
 	var thumbnail = document.createElement('div');
 	thumbnail.className = 'thumbnail';
 
@@ -43,16 +61,37 @@ for(var i = 0; i < shoes.length; i++) {
 
 	var caption = document.createElement('div');
 	caption.className = 'caption';
-	var label = document.createElement('h2');
+
+	var label = document.createElement('h3');
 	var label_text = document.createTextNode(shoes[i].name);
-	caption.appendChild(label);
+	label.appendChild(label_text);
 
 	var descr = document.createElement('p');
 	var descr_text = document.createTextNode(shoes[i].descr);
+	descr.appendChild(descr_text);
 
 	var rating = document.createElement('i');
 
 	var reviews = document.createElement('span');
 	var reviews_text = document.createTextNode(shoes[i].reviews + ' reviews');
 	reviews.appendChild(reviews_text);
+
+	var button_div = document.createElement('div');
+
+	var buy = document.createElement('button');
+	var button_text = document.createTextNode('Buy Now');
+	buy.appendChild(button_text);
+	buy.className = 'btn btn-default'
+
+	wrapper.appendChild(innerdiv);
+	innerdiv.appendChild(thumbnail);
+	thumbnail.appendChild(img);
+	thumbnail.appendChild(caption);
+	caption.appendChild(label);
+	caption.appendChild(descr);
+	caption.appendChild(rating);
+	caption.appendChild(reviews);
+	button_div.appendChild(buy);
+	caption.appendChild(button_div);
+	document.getElementById('body').appendChild(wrapper);
 }
